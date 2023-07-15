0765129dab6e66bd2b27fe4ef42cc5b497e653b6
Jhonatan Mota <jhonatan.mota@pavemobility.com>
2023-07-15 17:32:44 -0300

    greetings function

--- main.go
+++ main.go
@@ -5,5 +5,12 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	name := "Jhonatan Mota"
+
+	fmt.Println(Hello(name))
+}
+
+func Hello(name string) string {
+	message := fmt.Sprintf("Hello %v. Welcome to my learning go journey!", name)
+	return message
 }
